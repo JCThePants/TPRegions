9305c34faf555492f254f42d3f0a9269d3456a3f
jcThePants <jcthepants@gmail.com>
2014-11-06 01:19:48 -0800

    refactor CommandArguments.getInt to getInteger

--- src/com/jcwhatever/bukkit/tpregions/commands/list/ListCommand.java
+++ src/com/jcwhatever/bukkit/tpregions/commands/list/ListCommand.java
@@ -55,7 +55,7 @@ public class ListCommand extends AbstractCommand {
 	@Override
 	public void execute(CommandSender sender, CommandArguments args) throws InvalidValueException {
 		
-		int page = args.getInt("page");
+		int page = args.getInteger("page");
 		
 		TPRegionManager regionManager = TPRegions.getInstance().getRegionManager();
 		
--- src/com/jcwhatever/bukkit/tpregions/commands/list/PortalsSubCommand.java
+++ src/com/jcwhatever/bukkit/tpregions/commands/list/PortalsSubCommand.java
@@ -50,7 +50,7 @@ public class PortalsSubCommand extends AbstractCommand {
 	@Override
 	public void execute(CommandSender sender, CommandArguments args) throws InvalidValueException {
 		
-		int page = args.getInt("page");
+		int page = args.getInteger("page");
 		
 		TPRegionManager regionManager = TPRegions.getInstance().getRegionManager();
 		
